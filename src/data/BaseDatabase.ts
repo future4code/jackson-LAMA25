8f592d9857d7548e37c7fd6f7d72e364bc6cf400
Mayara Araujo <mayaramaaraujo98@gmail.com>
2020-11-26 19:32:00 -0400

    adição de services e conexão com banco

---
+++ src/data/BaseDatabase.ts
@@ -0,0 +1,49 @@
+import knex from 'knex'
+import dotenv from 'dotenv'
+import Knex from 'knex'
+
+dotenv.config()
+
+export abstract class BaseDatabase {
+    private static connection: Knex | null = null
+  
+    protected getConnection(): Knex {
+        if(!BaseDatabase.connection){
+            BaseDatabase.connection = knex({
+                client: "mysql",
+                connection:{
+                    host: process.env.DB_HOST,
+                    port: 3306,
+                    user: process.env.DB_USER,
+                    password: process.env.DB_PASSWORD,
+                    database: process.env.DB_DATABASE_NAME,
+                },
+            });
+        }
+
+        return BaseDatabase.connection;
+    }
+
+    public static async destroyConnection(): Promise<void> {
+        if(BaseDatabase.connection){
+            await BaseDatabase.connection.destroy();
+            BaseDatabase.connection = null;
+        }
+    }
+
+    protected convertIntToBoolean(value: number): boolean{
+        return value === 1;
+    }
+}
+
+/*export const connection = knex({
+    client: 'mysql',
+    connection: {
+       host: process.env.DB_HOST,
+       user: process.env.DB_USER,
+       password: process.env.DB_PASSWORD,
+       database: process.env.DB_NAME,
+       port: 3306
+    }
+})
+*/
